6b1b34abe24df53c75fc0f9b994ef13f32f4ade6
TechBuilderTeam <saashid18@gmail.com>
2024-10-18 01:21:33 +0600

    updated caurosel

--- src/app/(commonlayout)/component/pages/home/HomeCarosel.tsx
+++ src/app/(commonlayout)/component/pages/home/HomeCarosel.tsx
@@ -2,11 +2,11 @@
 import React, { useCallback } from 'react'
 import useEmblaCarousel from 'embla-carousel-react'
 import Autoplay from 'embla-carousel-autoplay'
-import { Image } from '@nextui-org/react';
 
 const HomeCarosel = () => {
     const [emblaRef, emblaApi] = useEmblaCarousel({ loop: true }, [
         Autoplay({ delay: 2000 })])
+        
     const scrollPrev = useCallback(() => {
         if (emblaApi) emblaApi.scrollPrev()
     }, [emblaApi])
@@ -14,39 +14,76 @@ const HomeCarosel = () => {
     const scrollNext = useCallback(() => {
         if (emblaApi) emblaApi.scrollNext()
     }, [emblaApi])
+
     return (
         <div className="embla relative" >
             
             <div className="embla__viewport mt-12 h-80 md:h-[70vh] w-full border" ref={emblaRef}>
                 <div className='embla__container h-full'>
                     <div className="embla__slide flex items-center justify-center">
-                        <Image alt='slide1' src={`../../../../../../public/assets/slide1.jpg`} 
-                        className="w-[100vh]"/>
+                        <div
+                            className='relative w-full h-[80vh] md:h-[70vh]'
+                            style={{
+                                backgroundImage: `url('/assets/slide1.jpg')`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                            }}
+                        >
+                            <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 flex items-center justify-center bg-red-50 bg-transparent">
+                                <h1 className='text-3xl md:text-5xl font-bold text-center my-2'>Welcome to Recipe Hunt</h1>
+                            </div>
+                        </div>
                     </div>
+
+         
                     <div className="embla__slide flex items-center justify-center">
-                        <Image alt='slide2' src={`../../../../../../public/assets/slide2.jpg`}  className="w-[100vh]"/>
+                        <div
+                            className='relative w-full h-[80vh] md:h-[70vh]'
+                            style={{
+                                backgroundImage: `url('/assets/slide2.jpg')`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                            }}
+                        >
+                             <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 flex items-center justify-center bg-red-50 bg-transparent">
+                                <h1 className='text-3xl md:text-5xl font-bold text-center my-2'>Welcome to Recipe Hunt</h1>
+                            </div>
+                        </div>
                     </div>
+
+                
                     <div className="embla__slide flex items-center justify-center">
-                        <Image alt='slide3' src={`../../../../../../public/assets/slide3.jpg`}  className="w-[100vh]"/>
+                        <div
+                            className='relative w-full h-[80vh] md:h-[70vh]'
+                            style={{
+                                backgroundImage: `url('/assets/slide3.jpg')`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                            }}
+                        >
+                              <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 flex items-center justify-center bg-red-50 bg-transparent">
+                                <h1 className='text-3xl md:text-5xl font-bold text-center my-2'>Welcome to Recipe Hunt</h1>
+                            </div>
+                        </div>
                     </div>
-                 
+
                 </div>
             </div>
-            <div className='absolute top-1/2 font-bold'>
-             <div className='flex justify-between items-center'>
-             <button className="embla__prev px-4" onClick={scrollPrev}>
+
+            {/* Navigation buttons */}
+            <div className='absolute top-1/2 w-full flex justify-between items-center'>
+                <button className="embla__prev px-4" onClick={scrollPrev}>
                     Previous
                 </button>
-                <button className="embla__next ml-52 md:ml-[174vh]" onClick={scrollNext}>
+                <button className="embla__next px-4" onClick={scrollNext}>
                     Next
                 </button>
-             </div>
             </div>
         </div>
     )
-};
+}
 
 export default HomeCarosel;
-
-
-
