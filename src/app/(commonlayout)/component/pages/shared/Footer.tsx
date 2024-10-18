f4d5e7eeddb123848cfeb07901682baee3767301
TechBuilderTeam <saashid18@gmail.com>
2024-10-19 00:16:43 +0600

    footer done

--- src/app/(commonlayout)/component/pages/shared/Footer.tsx
+++ src/app/(commonlayout)/component/pages/shared/Footer.tsx
@@ -1,11 +1,51 @@
 "use client";
 
-const Footer = () => {
-    return (
-        <div className="text-center mt-10">
-            <h1 className="text-red-500 text-3xl" > Footer</h1>
-        </div>
-    );
-};
+import { Image, Link } from '@nextui-org/react';
+import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 
-export default Footer;
+
+export default function Footer() {
+  return (
+    <div>
+      <div className="py-6">
+        <div className="max-w-6xl mx-auto">
+          <div className="flex flex-col md:flex-row justify-between items-center">
+            <div className="flex flex-col items-center mb-4 md:mb-0">
+              <Link href="/">
+                <div className="flex items-center">
+                  <Image src="/assets/footerlogo.png" className="h-14 md:h-28" alt="Logo" />
+                </div>
+              </Link>
+              <p className="mb-2 text-sm">&copy; 2024 Recipe Hunt. All rights reserved.</p>
+            </div>
+            < div className='flex flex-col justify-between items-center gap-4 my-10'>
+              <div className="flex space-x-4 mb-4 md:mb-0 text-red-500">
+                <a href="/about" className="hover:text-white underline">
+                  About
+                </a>
+                <a href="/contact" className="hover:text-white underline">
+                  Contact
+                </a>
+                <a href="/privacy" className="hover:text-white underline">
+                  Privacy Policy
+                </a>
+              </div>
+              {/* Social Links */}
+              <div className="flex space-x-4 text-red-500">
+                <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+                  <FaFacebookF size={20} />
+                </a>
+                <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+                  <FaTwitter size={20} />
+                </a>
+                <a href="https://www.linkedin.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
+                  <FaLinkedinIn size={20} />
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
