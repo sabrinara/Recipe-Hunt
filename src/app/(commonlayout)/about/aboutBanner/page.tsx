43050a1bf66a22151dcd62c572a1c605ce4ae0a4
TechBuilderTeam <saashid18@gmail.com>
2024-11-01 23:11:11 +0600

    about page

---
+++ src/app/(commonlayout)/about/aboutBanner/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const AboutBanner = () => {
+    return (
+        <div>
+        
+            <h1>About Banner</h1>
+        </div>
+    );
+};
+
+export default AboutBanner;
---
+++ src/app/(commonlayout)/about/page.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import AboutBanner from './aboutBanner/page';
+
+const About = () => {
+    return (
+        <div>
+            <AboutBanner/>
+            <h1>About</h1>
+        </div>
+    );
+};
+
+export default About;
