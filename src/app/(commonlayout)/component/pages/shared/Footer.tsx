8ed4482bb755b1fb77912f0bee3715b3098debc6
TechBuilderTeam <saashid18@gmail.com>
2024-11-08 23:52:18 +0600

    footer design change

--- src/app/(commonlayout)/component/pages/shared/Footer.tsx
+++ src/app/(commonlayout)/component/pages/shared/Footer.tsx
@@ -7,7 +7,13 @@ import { FaFacebookF, FaTwitter, FaLinkedinIn } from 'react-icons/fa';
 export default function Footer() {
   return (
     <div>
-      <div className="py-6">
+      <div className="py-16"   style={{
+                                backgroundImage: `url('/assets/footer.jpg')`,
+                                backgroundSize: 'cover',
+                                backgroundPosition: 'center',
+                                backgroundRepeat: 'no-repeat',
+                                opacity: "85%"
+                            }}>
         <div className="max-w-6xl mx-auto">
           <div className="flex flex-col md:flex-row justify-between items-center">
             <div className="flex flex-col items-center mb-4 md:mb-0">
@@ -16,10 +22,10 @@ export default function Footer() {
                   <Image src="/assets/footerlogo.png" className="h-14 md:h-28" alt="Logo" />
                 </div>
               </Link>
-              <p className="mb-2 text-sm">&copy; 2024 Recipe Hunt. All rights reserved.</p>
+              <p className="mb-2 text-sm text-red-700 font-bold">&copy; 2024 Recipe Hunt. All rights reserved.</p>
             </div>
             < div className='flex flex-col justify-between items-center gap-4 my-10'>
-              <div className="flex space-x-4 mb-4 md:mb-0 text-red-500">
+              <div className="flex space-x-4 mb-4 md:mb-0 text-white font-bold">
                 <a href="/about" className="hover:text-white underline">
                   About
                 </a>
@@ -31,7 +37,7 @@ export default function Footer() {
                 </a>
               </div>
               {/* Social Links */}
-              <div className="flex space-x-4 text-red-500">
+              <div className="flex space-x-4 text-red-700">
                 <a href="https://www.facebook.com" target="_blank" rel="noopener noreferrer" className="hover:text-white">
                   <FaFacebookF size={20} />
                 </a>
