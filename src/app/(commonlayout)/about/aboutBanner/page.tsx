67a3e484c2f076cbe2fa7eecf4cdbbd685268091
Sabrina_Rashid <saashid18@gmail.com>
2025-04-17 23:45:55 +0600

    about banner is ready

--- src/app/(commonlayout)/about/aboutBanner/page.tsx
+++ src/app/(commonlayout)/about/aboutBanner/page.tsx
@@ -1,10 +1,26 @@
-import React from 'react';
+
 
 const AboutBanner = () => {
     return (
-        <div>
-        
-            <h1>About Banner</h1>
+        <div className="mx-auto container">
+        <div
+                className="relative w-full h-[60vh] mb-6 rounded-md overflow-hidden"
+            >
+                <div
+                    className="absolute inset-0"
+                    style={{
+                        backgroundImage: `url('/assets/bannersm.jpg')`,
+                        backgroundSize: "cover",
+                        backgroundPosition: "center",
+                        backgroundRepeat: "no-repeat",
+                        filter: "blur(2px)",
+                    }}
+                ></div>
+                <div className="absolute top-1/2 left-0 right-0 transform -translate-y-1/2 flex justify-center px-4 py-4 mx-96 bg-white/80">
+                  <h1 className="text-md md:text-2xl font-semibold text-[#E10101]">Home &gt;&gt;&gt; About</h1>
+                </div>
+              
+            </div>
         </div>
     );
 };
